312014887c1f6bb0a2d2a007884934b5bf89349c
leunami420 <marconich311@gmail.com>
2023-04-19 19:16:21 +0200

    heya

---
+++ Part2.py
@@ -0,0 +1,11 @@
+list1 = []
+
+for i in range(1,21):
+    list1.append(i)
+print(list1)
+
+squaresOfList1 = []
+
+for x in range(0,20):
+    squaresOfList1.append(list1[x]**2)
+print(squaresOfList1)
