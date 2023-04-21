deb4823f615545e687b00d300e5c0837ed753251
ttthien02 <mshientran02@gmail.com>
2023-04-21 12:35:32 +0200

    new change

--- Part2.py
+++ Part2.py
@@ -1,20 +1,21 @@
+#a list between 1 and 20
 list1 = []
 
 for i in range(1,21):
     list1.append(i)
 print(list1)
 
-#part2.1
+#aufgabe1.1
 squaresOfList1 = []
 
 for x in range(0,20):
     squaresOfList1.append(list1[x]**2)
 print(squaresOfList1)
 
-#part2.2
+#aufgabe1.2
 evenList = []
 
-for numbers in range(1,21):
+for numbers in list1:
     if numbers % 2 == 0:
         evenList.append(numbers)
 print(evenList)
