3e48e8f80cec4f72e41f4a7231f74f7a3352b494
ttthien02 <mshientran02@gmail.com>
2023-04-21 12:27:57 +0200

    just add a new part

--- Part2.py
+++ Part2.py
@@ -4,10 +4,21 @@ for i in range(1,21):
     list1.append(i)
 print(list1)
 
+#part2.1
 squaresOfList1 = []
 
 for x in range(0,20):
     squaresOfList1.append(list1[x]**2)
 print(squaresOfList1)
 
-Test
+#part2.2
+evenList = []
+
+for numbers in range(1,21):
+    if numbers % 2 == 0:
+        evenList.append(numbers)
+print(evenList)
+
+
+
+
